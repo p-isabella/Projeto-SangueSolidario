472adea746972fee37e98531ea025296f7419d2f
unknown <lucasdossantos2612@gmail.com>
2026-06-24 08:45:30 -0300

    Done Strategy

--- SangueSolidarioBackEnd/ValidaDadosDoador.py
+++ SangueSolidarioBackEnd/ValidaDadosDoador.py
@@ -2,7 +2,7 @@ from ServicoDoador import ServicoDoador
 
 class ValidaDadosDoador(ServicoDoador):
     # Algoritmo concreto de Validação dos Dados
-    
+
     def __init__(self, doador=None):
         super().__init__()
         self.__doador = doador
