db0f1774260926c445006059a8bc9a0ba07ddf9d
Victor Moreira <victormoreira247@gmail.com>
2026-07-01 03:48:57 -0300

    Refactor databaseAdapter for improved queries and tables

--- frontend/databaseAdapter.py
+++ frontend/databaseAdapter.py
@@ -5,39 +5,41 @@ class databaseAdapter:
   def __init__(self):
     self.init_table()
     
-  def query(self, query):
+  def query(self, query, params=None):
     connection = sqlite3.connect('banco.db')
     cursor = connection.cursor()
     print(query)
-    cursor.execute(*query)
+    if params:
+      cursor.execute(query,params)
+    else:
+      cursor.execute(query)
     resultado = cursor.fetchall()
     connection.commit()
     connection.close()
     return resultado
   
   def init_table(self):
-    self.query("CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, email TEXT UNIQUE, senha TEXT)")
-    self.query("""CREATE TABLE IF NOT EXISTS agendamentos (id TEXT PRIMARY KEY AUTOINCREMENT, usuario_id INTEGER, data TEXT NOT NULL, hora TEXT NOT NULL,
-    status TEXT NOT NULL DEFAULT 'pendente', FOGEIGN KEY (usuario_id) REFERENCES usuarios(id) )""")
+    self.query("CREATE TABLE IF NOT EXISTS usuarios (usuario_id INTEGER PRIMARY KEY AUTOINCREMENT, nome_completo TEXT, cpf TEXT UNIQUE, tipo_sanguineo TEXT, data_nascimento TEXT, cep TEXT, email TEXT UNIQUE, senha TEXT)")
+    self.query("""CREATE TABLE IF NOT EXISTS agendamentos (usuario_id INTEGER PRIMARY KEY AUTOINCREMENT, data TEXT NOT NULL, hora TEXT NOT NULL, unidade TEXT, status TEXT NOT NULL DEFAULT 'pendente', FOREIGN KEY (usuario_id) REFERENCES usuarios(id) )""")
     
   """ Queries de Usuário """
 
-  def push_usuario(self, nome, email, senha):
-    self.query(["INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?, ?)", (nome, email, senha)])
+  def push_usuario(self, nome_completo, cpf, tipo_sanguineo, data_nascimento, cep,  email, senha):
+    self.query(["INSERT INTO usuarios (nome_completo, cpf, tipo_sanguineo, data_nascimento, cep, email, senha) VALUES (?, ?, ?, ?, ?, ?, ?)", (nome_completo, cpf, tipo_sanguineo, data_nascimento, cep, email, senha)])
 
   def selectBy_usuario(self, id):
-    self.query(["SELECT * FROM usuarios WHERE id = ?", (id, )])
+    return self.query(["SELECT * FROM usuarios WHERE id = ?", (id, )])
 
   def put_usuario(self, id, nome, email, senha):
-    self.query(["UPDATE usuarios SET nome = ?, email = ?, senha = ? WHERE id = ?", (nome, email, senha, id)])
+    self.query(["UPDATE usuarios SET nome_completo = ?, email = ?, senha = ? WHERE id = ?", (nome, email, senha, id)])
 
   def delete_usuario(self, id):
-    self.query(["DELETE FROM usuarios WHERE id = ?", (id,)])
+    self.query(["DELETE FROM usuarios WHERE id = ?", (id, )])
 
   """ Queries de Agendamento """
 
-  def push_agendamento(self, id, usuario_id, data, hora, unidade, status):
-    self.query(["INSERT INTO agendamentos (id, usuario_id, data, hora, unidade, status) VALUES (?, ?, ?, ?)", (id, usuario_id, data, hora, unidade, status)])
+  def push_agendamento(self, usuario_id, data, hora, unidade, status):
+    self.query(["INSERT INTO agendamentos (usuario_id, data, hora, unidade, status) VALUES (?, ?, ?, ?)", (id, usuario_id, data, hora, unidade, status)])
 
   def selectBy_agendamento(self, id):
     self.query(["SELECT * FROM agendamentos WHERE id = ?", (id, )])
